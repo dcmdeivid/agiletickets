b7dbe29344f2ae93ab6c94e0af6206a374a0716b
Deivid Martins <dcmdeivid@gmail.com>
2011-08-27 13:22:28 -0300

    separado método de cria estabelecimento e cria espetáculo para maior legibilidade

--- src/main/java/br/com/caelum/agiletickets/PreencheBanco.java
+++ src/main/java/br/com/caelum/agiletickets/PreencheBanco.java
@@ -18,43 +18,63 @@ public class PreencheBanco {
 		EntityManager manager = managerCreator.getInstance();
 
 		manager.getTransaction().begin();
+		
 		limpaBanco(manager);
+		
 		Estabelecimento estabelecimento = criaEstabelecimento(manager);
 		
 		Espetaculo espetaculo = criaEspetaculo(manager, estabelecimento);
 
-		for (int i = 0; i < 10; i++) {
-			Sessao sessao = new Sessao();
-			sessao.setEspetaculo(espetaculo);
-			sessao.setInicio(new DateTime().plusDays(7+i));
-			sessao.setDuracaoEmMinutos(60 * 3);
-			sessao.setTotalIngressos(10);
-			sessao.setIngressosReservados(10 - i);
-			manager.persist(sessao);
-		}
+		criaSessoesParaEspetaculo(manager, espetaculo);
 
 		manager.getTransaction().commit();
 		manager.close();
 	}
 
+	private static void criaSessoesParaEspetaculo(EntityManager manager,
+			Espetaculo espetaculo) {
+		for (int i = 0; i < 10; i++) {
+			Sessao sessao = proximaSessao(espetaculo, i);
+			manager.persist(sessao);
+		}
+	}
+
+	private static Sessao proximaSessao(Espetaculo espetaculo, int i) {
+		Sessao sessao = new Sessao();
+		sessao.setEspetaculo(espetaculo);
+		sessao.setInicio(new DateTime().plusDays(7+i));
+		sessao.setDuracaoEmMinutos(60 * 3);
+		sessao.setTotalIngressos(10);
+		sessao.setIngressosReservados(10 - i);
+		return sessao;
+	}
+
 	private static Espetaculo criaEspetaculo(EntityManager manager,
+			Estabelecimento estabelecimento) {
+		Espetaculo espetaculo = populaEspetaculoInicial(estabelecimento);
+		manager.persist(espetaculo);
+		return espetaculo;
+	}
+
+	private static Espetaculo populaEspetaculoInicial(
 			Estabelecimento estabelecimento) {
 		Espetaculo espetaculo = new Espetaculo();
 		espetaculo.setEstabelecimento(estabelecimento);
 		espetaculo.setNome("Depeche Mode");
 		espetaculo.setTipo(TipoDeEspetaculo.SHOW);
-
-		
-		manager.persist(espetaculo);
 		return espetaculo;
 	}
 
 	private static Estabelecimento criaEstabelecimento(EntityManager manager) {
+		Estabelecimento estabelecimento = criaEstabelecimentoInicial();
+		manager.persist(estabelecimento);
+		return estabelecimento;
+	}
+
+	private static Estabelecimento criaEstabelecimentoInicial() {
 		Estabelecimento estabelecimento = new Estabelecimento();
 		estabelecimento.setNome("Casa de shows");
 		estabelecimento.setEndereco("Rua dos Silveiras, 12345");
-		
-		manager.persist(estabelecimento);
 		return estabelecimento;
 	}
 
